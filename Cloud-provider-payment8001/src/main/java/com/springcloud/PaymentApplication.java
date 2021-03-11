1a05b2d82d0acf3a01b1c1abdcead2f12d7a477b
lu101092924 <101092924@qq.com>
2021-03-11 16:35:04 +0800

    添加单机Eureka注册中心

--- Cloud-provider-payment8001/src/main/java/com/springcloud/PaymentApplication.java
+++ Cloud-provider-payment8001/src/main/java/com/springcloud/PaymentApplication.java
@@ -3,9 +3,12 @@ package com.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.springcloud.mapper.**")
+@EnableEurekaClient
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class,args);
--- cloud-eureka-server7001/src/main/java/com/springcloud/EurekaServerApplication.java
+++ cloud-eureka-server7001/src/main/java/com/springcloud/EurekaServerApplication.java
@@ -2,12 +2,12 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class ConsumerApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class,args);
+        SpringApplication.run(EurekaServerApplication.class,args);
     }
 }

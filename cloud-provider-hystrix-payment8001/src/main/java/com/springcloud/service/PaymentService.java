ac9825a8db1dc0a4dde1476428dd2bb76676755d
lu101092924 <101092924@qq.com>
2021-03-19 17:51:34 +0800

    添加Hystrix相关模块

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/springcloud/service/PaymentService.java
@@ -0,0 +1,19 @@
+package com.springcloud.service;
+
+public interface PaymentService {
+    /**
+     * 正常访问
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfo_OK(Integer id);
+
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfo_TimeOut(Integer id);
+}

8368f7ed77db9f18f658a50a59108b98cf1a753b
lu101092924 <101092924@qq.com>
2021-03-29 15:19:56 +0800

    添加Stream消息驱动模块

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/springcloud/StreamMQ8802Application.java
@@ -0,0 +1,11 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamMQ8802Application {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQ8802Application.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/springcloud/StreamMQ8803Application.java
@@ -0,0 +1,11 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamMQ8803Application {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQ8803Application.class,args);
+    }
+}

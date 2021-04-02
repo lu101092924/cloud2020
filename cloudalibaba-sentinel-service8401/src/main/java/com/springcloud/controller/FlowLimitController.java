bde7d2a42f2dce0487d90bbf7f1f463416f50882
lu101092924 <101092924@qq.com>
2021-04-02 18:01:33 +0800

    添加Sentinel测试模块，测试熔断策略RT

--- cloudalibaba-sentinel-service8401/src/main/java/com/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/springcloud/controller/FlowLimitController.java
@@ -1,9 +1,13 @@
 package com.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@Slf4j
 public class FlowLimitController
 {
     @GetMapping("/testA")
@@ -15,6 +19,15 @@ public class FlowLimitController
     public String testB() {
 
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        log.info("testD 测试RT");
+
+        return "------testD";
     }
 
 
